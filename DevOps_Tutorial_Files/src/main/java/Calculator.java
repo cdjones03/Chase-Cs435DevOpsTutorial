996a558d7fb7fab4b8f70388f4141f7d497bc609
Chase Jones <cdjones03@email.wm.edu>
2020-10-09 16:27:41 -0400

    Calculator Attempt 2

--- DevOps_Tutorial_Files/src/main/java/Calculator.java
+++ DevOps_Tutorial_Files/src/main/java/Calculator.java
@@ -23,7 +23,7 @@ public class Calculator{
     }
 
     public double power(double x, int power){
-        return Math.pow(a, power);
+        return Math.pow(x, power);
     }
 
     public static void main(String args[]){
